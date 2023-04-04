8e334fef6663fa94dd7bb6c4105f3e89e383f97c
Hoki wahyuono pranto <79401008+yuurha12@users.noreply.github.com>
2023-04-04 10:31:37 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const API = axios.create({
 //     baseURL: process.env.REACT_APP_BASEURL
-    baseURL: "https://api2.chaostic-hoki.my.id:5005/api/v1/",
+    baseURL: "https://api2.chaostic-hoki.my.id/api/v1/",
 });
 
     export const setAuthToken = (token) => {
